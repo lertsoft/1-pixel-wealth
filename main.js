1c3eb60e98b77ac6601f635a95d6b233ae5db222
Ronny Coste <ronny21.97@gmail.com>
2022-06-30 21:50:18 -0400

    Fixed Links

--- main.js
+++ main.js
@@ -110,4 +110,40 @@ function toggleZoom() {
   document.getElementById('line-chart').classList.toggle('zoom');
 }
 
+// I18Next for internazionalization of the website.
+i18next
+  .use(i18nextHttpBackend)
+  .use(i18nextBrowserLanguageDetector)
+  .init({
+    fallbackLng: 'en',
+    debug: true,
+    ns: ['special', 'common'],
+    defaultNS: 'special',
+    backend: {
+      // load from i18next-gitbook repo
+      loadPath: 'https://raw.githubusercontent.com/i18next/i18next-gitbook/master/locales/{{lng}}/{{ns}}.json',
+      crossDomain: true
+    }
+  }, function(err, t) {
+    // init set content
+    updateContent();
+  });
+
+
+// just set some content and react to language changes
+// could be optimized using vue-i18next, jquery-i18next, react-i18next, ...
+function updateContent() {
+  document.getElementById('title').innerHTML = i18next.t('title', { what: 'i18next' });
+  document.getElementById('saveBtn').innerHTML = i18next.t('common:button.save', { count: Math.floor(Math.random()*2+1)  });
+  
+  document.getElementById('info').innerHTML = `detected user language: "${i18next.language}"  --> loaded languages: "${i18next.languages.join(', ')}"`;
+}
+
+function changeLng(lng) {
+  i18next.changeLanguage(lng);
+}
+
+i18next.on('languageChanged', () => {
+  updateContent();
+});
 
